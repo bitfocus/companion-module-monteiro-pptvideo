54122280f0db6f58c0064fd48b1d719495dabf3b
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-07-02 23:12:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,25 +4,12 @@ const REGEX_IP_OR_HOST =
 	'/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})$|^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]).)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9]))$/'
 
 export const ConfigFields = [
-	{
-		type: 'static-text',
-		id: 'info',
-		label: 'Information',
-		width: 12,
-		value: `
-					<h3>IMPORTANT MESSAGE</h3>
-					<div>
-						<strong>Please read and understand the following before using this module</strong>
-						<br>
-						This software aims to use the companion feature to use more than one computer on the network and be able to control the opening of video and ppt simultaneously.<p>
-            
-					</div>
-			`,
-	},
+
 	{
 		type: 'textinput',
 		id: 'host',
 		label: 'Target Host name or IP',
+		default: '127.0.0.1',
 		width: 8,
 		regex: REGEX_IP_OR_HOST,
 	},
@@ -30,6 +17,7 @@ export const ConfigFields = [
 		type: 'textinput',
 		id: 'port',
 		label: 'Target Port',
+		default: '7800',
 		width: 4,
 		regex: Regex.PORT,
 	},
