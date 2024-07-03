427f4200e13a32ef91b497d1031ab2a48b25c51c
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-07-02 23:11:38 -0300

    Update actions.js

--- actions.js
+++ actions.js
@@ -2,60 +2,32 @@ export function getActionDefinitions(self) {
 
 	const actions = {}
 
-	actions['send'] = {
-		name: 'PPT/VIDEO',
-		options: [
-			{
-				type: 'textinput',
-				id: 'id_send',
-				label: 'Command:',
-				tooltip: 'Use %hh to insert Hex codes\nObsolete, use Send HEX command instead',
-				default: '',
-				useVariables: true,
-			},
-		],
-		callback: async (action) => {
-			const cmd = unescape(await self.parseVariablesInString(action.options.id_send))
-
-			if (cmd != '') {
-				const sendBuf = Buffer.from(cmd, 'latin1')
-
-				self.log('debug', 'sending to ' + self.config.host + ': ' + sendBuf.toString())
-
-				if (self.socket !== undefined && self.socket.isConnected) {
-					self.socket.send(sendBuf)
-				} else {
-					self.log('debug', 'Socket not connected :(')
-				}
-			}
-		},
-	}
-
-	// Helper function to create simple command actions
 	const createSimpleCommandAction = (name, command) => ({
 		name: `${name}`,
 		options: [],
 		callback: async () => {
 			const cmd = command
 			const sendBuf = Buffer.from(cmd, 'latin1')
-
-			self.log('debug', `sending to ${self.config.host}: ${sendBuf.toString()}`)
-
-			if (self.socket !== undefined && self.socket.isConnected) {
-				self.socket.send(sendBuf)
-			} else {
-				self.log('debug', 'Socket not connected :(')
-			}
+			self.socket.send(sendBuf)
 		},
 	})
 
-	// Creating simple command actions
 	const simpleCommands = {
 		ESC: 'esc',
 		NEXT: 'next',
 		PREVIOUS: 'previous',
 		EXIT_PPT: 'pptexit',
 		EXIT_VIDEO: 'videoexit',
+	}
+
+	for (let i = 1; i <= 20; i++) {
+		const pptCommand = `PPT${i.toString().padStart(2, '0')}`;
+		simpleCommands[pptCommand] = pptCommand.toLowerCase();
+	}
+
+	for (let i = 1; i <= 10; i++) {
+		const videoCommand = `VIDEO${i.toString().padStart(2, '0')}`;
+		simpleCommands[videoCommand] = videoCommand.toLowerCase();
 	}
 
 	for (const [name, command] of Object.entries(simpleCommands)) {
