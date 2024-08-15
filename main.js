36664b655651be2c2345130d3c69d72bd01a3af8
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-08-15 11:28:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+import { InstanceBase, InstanceStatus, runEntrypoint, TCPHelper } from '@companion-module/base'
+import { ConfigFields } from './config.js'
+import { getActionDefinitions } from './actions.js'
+import { getPresetDefinitions } from './presets.js'
+
+class PptVideoTcp extends InstanceBase {
+	async init(config) {
+		this.config = config
+
+
+		this.setActionDefinitions(getActionDefinitions(this))
+		this.setPresetDefinitions(getPresetDefinitions())
+		await this.configUpdated(config)
+	}
+
+	async configUpdated(config) {
+		if (this.socket) {
+			this.socket.destroy()
+			delete this.socket
+		}
+
+		this.config = config
+		this.init_tcp()
+	}
+
+	async destroy() {
+		if (this.socket) {
+			this.socket.destroy()
+		} else {
+			this.updateStatus(InstanceStatus.Disconnected)
+		}
+	}
+
+	getConfigFields() {
+		return ConfigFields
+	}
+
+	init_tcp() {
+		if (this.socket) {
+			this.socket.destroy()
+			delete this.socket
+		}
+
+		this.updateStatus(InstanceStatus.Connecting)
+
+		if (this.config.host) {
+			this.socket = new TCPHelper(this.config.host, this.config.port)
+
+			this.socket.on('status_change', (status, message) => {
+				this.updateStatus(status, message)
+			})
+
+			this.socket.on('error', (err) => {
+				this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
+				this.log('error', 'Network error: ' + err.message)
+			})
+		} else {
+			this.updateStatus(InstanceStatus.BadConfig)
+		}
+	}
+
+}
+
+runEntrypoint(PptVideoTcp, [])
