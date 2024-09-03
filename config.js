61e3a5cfece90d55900c8d8ecd0918697f5f96fb
MonteiroVideo <166059439+MonteiroVideo@users.noreply.github.com>
2024-09-03 19:19:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,12 @@ const REGEX_IP_OR_HOST =
 	'/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})$|^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]).)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9]))$/'
 
 export const ConfigFields = [
-{
+	{
 		type: 'static-text',
 		id: 'info',
 		label: 'Information', 
 		width: 12,
-		value: 'I put the download link in the Help folder, but for information purposes. This module consists of enabling PowerPoint and video control through buttons.
-Facilitating the manipulation of more than one presentation, you can control it on more than one computer using the software and the companion, being able to open, close, advance and other functions on more than one computer simultaneously.'
+		value: 'I put the download link in the Help folder, but for information purposes. This module consists of enabling PowerPoint and video control through buttons. Facilitating the manipulation of more than one presentation, you can control it on more than one computer using the software and the companion, being able to open, close, advance and other functions on more than one computer simultaneously.'
 	},
 	{
 		type: 'textinput',
